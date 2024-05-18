189058876f114ac96f18a212674f69fff6d32795
gauravidya <gaurav.meena@vidyagxp.com>
2024-05-18 16:34:31 +0530

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,229 +4,220 @@ import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faFacebookF,
-    faInstagram,
-    faLinkedinIn,
-    faTwitter,
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import {
-    faAngleRight,
-    faEnvelopeOpenText,
-    faPhone,
-    faSignsPost,
+  faAngleRight,
+  faEnvelopeOpenText,
+  faPhone,
+  faSignsPost,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
-    const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-    const handleButtonClick = () => {
-        console.log("setIsButtonClicked clicked");
-        if (isButtonClicked) {
-            setIsButtonClicked(false);
-        } else {
-            setIsButtonClicked(true);
-        }
-    };
+  const handleButtonClick = () => {
+    console.log("setIsButtonClicked clicked");
+    if (isButtonClicked) {
+      setIsButtonClicked(false);
+    } else {
+      setIsButtonClicked(true);
+    }
+  };
 
-    return (
-        <>
-            <div className="header-section">
-                <div className="container-fluid">
-                    <div className="top-nav d-flex pt-2 justify-content-end">
-                        <div>
-                            <FontAwesomeIcon icon={faEnvelopeOpenText} className="me-3 text-white" />
-                            <span className="text-white fw-bolder">info@vidyagxp.com</span>
-                        </div>
-                        <div className="ms-5">
-                            <FontAwesomeIcon icon={faPhone} className="me-3 text-white" />
-                            <span className="text-white fw-bolder">+91-8821836528</span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="header-section">
+        <div className="container-fluid">
+          <div className="top-nav d-flex pt-2 justify-content-end">
+            <div>
+              <FontAwesomeIcon icon={faEnvelopeOpenText} className="me-3 text-white" />
+              <span className="text-white fw-bolder">info@vidyagxp.com</span>
             </div>
-
-            <div id="responsive-headerdrop">
-                {isButtonClicked && (
-                    <div className="nav-item">
-                        <div className="m-0">
-                            <Link className="responsive-item px-4" to="/">
-                                Home{" "}
-                            </Link>
-                            <Link className="responsive-item  px-4" to="/about">
-                                About{" "}
-                            </Link>
-                            <Link className="responsive-item  px-4" to="/services">
-                                Services
-                            </Link>
-                            <Link className="responsive-item  px-4" to="/industries">
-                                Industries
-                            </Link>
-                            <Link className="responsive-item  px-4" to="/features">
-                                Features
-                            </Link>
-                            <Link className="responsive-item  px-4" to="/contact">
-                                Contact Us
-                            </Link>
-                        </div>
-                    </div>
-                )}
+            <div className="ms-5">
+              <FontAwesomeIcon icon={faPhone} className="me-3 text-white" />
+              <span className="text-white fw-bolder">+91-8821836528</span>
             </div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-                <div className="header-item-flex">
-                    <div className="brand-adjust">
-                        <Link className="navbar-brand" to="/">
-                            <img src="vidyaGxp_logo.png" height="65px" width="250px" className="" alt="" />
-                        </Link>
-                    </div>
-                    <div className="collapse navbar-collapse pt-3 " id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <NavLink className="nav-link" to="/" activeClassName="active">
-                                    Home <span className="sr-only">(current)</span>
-                                </NavLink>
-                            </li>
+          </div>
+        </div>
+      </div>
 
-                            <li className="nav-item">
-                                <NavLink
-                                    to="/about"
-                                    className="nav-link"
-                                    activeClassName="active"
-                                    tabIndex="-1"
-                                    aria-disabled="true"
-                                >
-                                    About
-                                </NavLink>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link
-                                    className="nav-link dropdown-toggle"
-                                    to="#"
-                                    id="navbarDropdown"
-                                    role="button"
-                                    data-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                >
-                                    Services
-                                </Link>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <Link className="dropdown-item" to="/ebmr">
-                                        e-BMR
-                                    </Link>
-                                    <Link className="dropdown-item" to="/elogbook">
-                                        e-LogBook
-                                    </Link>
-                                    <Link className="dropdown-item" to="/lms">
-                                        LMS
-                                    </Link>
-                                    <Link className="dropdown-item" to="/mes">
-                                        MES
-                                    </Link>
-                                    <Link className="dropdown-item" to="/edms">
-                                        EDMS
-                                    </Link>
-                                    <Link className="dropdown-item" to="/eqms">
-                                        EQMS
-                                    </Link>
-                                    <Link className="dropdown-item" to="/quality-risk-management">
-                                        Quality Risk Management
-                                    </Link>
-                                    <Link className="dropdown-item" to="/pharma-audit">
-                                        Pharma Audit & Remediation
-                                    </Link>
-                                    <Link className="dropdown-item" to="/root-cause-analysis">
-                                        Automated Root Cause Analysis
-                                    </Link>
-                                    <Link className="dropdown-item" to="/warehouse-management">
-                                        Warehouse management system
-                                    </Link>
-                                    <Link className="dropdown-item" to="/paperless-microbiology">
-                                        Paper less microbiology
-                                    </Link>
-                                    <Link className="dropdown-item" to="/usfda-warning-letter-management">
-                                        {" "}
-                                        USFDA warning letter management
-                                    </Link>
-                                    <Link className="dropdown-item" to="/merger-and-acquisition">
-                                        Merger & acquisition
-                                    </Link>
-                                    <Link className="dropdown-item" to="/ai-assited-culture-of-quality">
-                                        {" "}
-                                        AI-Assisted Culture of Quality
-                                    </Link>
-                                    <Link className="dropdown-item" to="/connected-and-integrated-gxp-systems">
-                                        {" "}
-                                        Connected & Integrated GXP Systems
-                                    </Link>
-                                    <Link className="dropdown-item" to="/ai-assited-pharma">
-                                        AI-Assisted Pharma 4.0
-                                    </Link>
-                                    <Link className="dropdown-item" to="/intelligent-pharma">
-                                        {" "}
-                                        Intelligent Pharma Manufacturing Factory
-                                    </Link>
-                                    <Link className="dropdown-item" to="/gxp-training">
-                                        GxP Training
-                                    </Link>
-                                    <Link className="dropdown-item" to="/rpa">
-                                        Robotic Process Automation (RPA){" "}
-                                    </Link>
-                                    <div className="dropdown-divider"></div>
-                                </div>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink
-                                    className="nav-link"
-                                    to="/industries"
-                                    tabIndex="-1"
-                                    aria-disabled="true"
-                                    activeClassName="active"
-                                >
-                                    Industries
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink
-                                    className="nav-link"
-                                    to="/features"
-                                    tabIndex="-1"
-                                    aria-disabled="true"
-                                    activeClassName="active"
-                                >
-                                    Features
-                                </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink
-                                    className="nav-link"
-                                    to="/contact"
-                                    tabIndex="-1"
-                                    aria-disabled="true"
-                                    activeClassName="active"
-                                    style={{width:'90px'}}
-                                >
-                                    Contact Us
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+      <div id="responsive-headerdrop">
+        {isButtonClicked && (
+          <div className="nav-item">
+            <div className="m-0">
+              <Link className="responsive-item px-4" to="/">
+                Home{" "}
+              </Link>
+              <Link className="responsive-item  px-4" to="/about">
+                About{" "}
+              </Link>
+              <Link className="responsive-item  px-4" to="/services">
+                Services
+              </Link>
+              <Link className="responsive-item  px-4" to="/industries">
+                Industries
+              </Link>
+              <Link className="responsive-item  px-4" to="/features">
+                Features
+              </Link>
+              <Link className="responsive-item  px-4" to="/contact">
+                Contact Us
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <div className="header-item-flex">
+          <div className="brand-adjust">
+            <Link className="navbar-brand" to="/">
+              <img src="vidyaGxp_logo.png" height="65px" width="250px" className="" alt="" />
+            </Link>
+          </div>
+          <div className="collapse navbar-collapse pt-3 " id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <NavLink className="nav-link" to="/" activeClassName="active">
+                  Home <span className="sr-only">(current)</span>
+                </NavLink>
+              </li>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                    onClick={handleButtonClick}
+              <li className="nav-item">
+                <NavLink
+                  to="/about"
+                  className="nav-link"
+                  activeClassName="active"
+                  tabIndex="-1"
+                  aria-disabled="true"
                 >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            </nav>
-        </>
-    );
+                  About
+                </NavLink>
+              </li>
+
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Services
+                </Link>
+
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <div>
+                    <Link className="dropdown-item" to="/ebmr">
+                      e-BMR
+                    </Link>
+                    <Link className="dropdown-item" to="/elogbook">
+                      e-LogBook
+                    </Link>
+                    <Link className="dropdown-item" to="/lms">
+                      LMS
+                    </Link>
+                    <Link className="dropdown-item" to="/mes">
+                      MES
+                    </Link>
+                    <Link className="dropdown-item" to="/edms">
+                      EDMS
+                    </Link>
+                    <Link className="dropdown-item" to="/eqms">
+                      EQMS
+                    </Link>
+                    <Link className="dropdown-item" to="/quality-risk-management">
+                      Quality Risk Management
+                    </Link>
+                    <Link className="dropdown-item" to="/pharma-audit">
+                      Pharma Audit & Remediation
+                    </Link>
+                    <Link className="dropdown-item" to="/root-cause-analysis">
+                      Automated Root Cause Analysis
+                    </Link>
+                    <Link className="dropdown-item" to="/warehouse-management">
+                      Warehouse management system
+                    </Link>
+                    <Link className="dropdown-item" to="/connected-and-integrated-gxp-systems">
+                      Connected & Integrated GXP Systems
+                    </Link>
+                    <Link className="dropdown-item" to="/ai-assited-pharma">
+                      AI-Assisted Pharma 4.0
+                    </Link>
+                    <Link className="dropdown-item" to="/intelligent-pharma">
+                      Intelligent Pharma Manufacturing Factory
+                    </Link>
+                    <Link className="dropdown-item" to="/gxp-training">
+                      GxP Training
+                    </Link>
+                    <Link className="dropdown-item" to="/rpa">
+                      Robotic Process Automation (RPA){" "}
+                    </Link>
+                  </div>
+
+         
+                  {/* <div className="dropdown-divider"></div> */}
+                </div>
+              </li>
+
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/industries"
+                  tabIndex="-1"
+                  aria-disabled="true"
+                  activeClassName="active"
+                >
+                  Industries
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/features"
+                  tabIndex="-1"
+                  aria-disabled="true"
+                  activeClassName="active"
+                >
+                  Features
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/contact"
+                  tabIndex="-1"
+                  aria-disabled="true"
+                  activeClassName="active"
+                  style={{ width: "90px" }}
+                >
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={handleButtonClick}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
