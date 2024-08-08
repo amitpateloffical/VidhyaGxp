a45a31959ec73e43554c9887b1e467b2353d5d0a
sandhyaprajapati7354 <sandhya.prajapati@vidyagxp.com>
2024-08-08 13:04:47 +0530

    inner pages design

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,40 @@ import Rpa from "./pages/services/Rpa";
 import Industries from "./pages/Industries";
 import { useEffect } from "react";
 import PharmaAudit from "../src/pages/services/PharmaAudit";
+import Feasibility from "./pages/services/Engineering/Feasibility";
+import SiteSelection from "./pages/services/Engineering/SiteSelection";
+import FacilityDesign from "./pages/services/Engineering/FacilityDesign";
+import DesignRisk from "./pages/services/Engineering/Design-Risk";
+import DetailedEngineering from "./pages/services/Engineering/Detailed-Engineering";
+import RegulatoryCompliance from "./pages/services/Engineering/Regulatory-Compliance";
+import ProjectManagement from "./pages/services/Engineering/Project-Management";
+import TechnologyEquipment from "./pages/services/Engineering/Technology-Equipment";
+import CommissioningQualification from "./pages/services/Engineering/CommissioningQualification";
+import QualityAssurance from "./pages/services/Engineering/QualityAssurance";
+import TrainingCapacityBuilding from "./pages/services/Engineering/TrainingCapacityBuilding";
+import ContractualCloseout from "./pages/services/Engineering/ContractualCloseout";
+import PostProjectReview from "./pages/services/Engineering/PostProjectReview";
+import SupplyChainManagement from "./pages/services/Engineering/SupplyChainManagement";
+import FinancialAnalysis from "./pages/services/Engineering/FinancialAnalysis";
+import UserRequirementsSpecification from "./pages/services/QualificationValidation/UserRequirementsSpecification";
+import DesignQualification from "./pages/services/QualificationValidation/DesignQualification";
+import FactorySiteAcceptance from "./pages/services/QualificationValidation/FactorySiteAcceptance";
+import InstallationQualification from "./pages/services/QualificationValidation/InstallationQualification";
+import OperationQualification from "./pages/services/QualificationValidation/OperationQualification";
+import StandardOperatingProcedure from "./pages/services/QualificationValidation/StandardOperatingProcedure";
+import PerformanceQualification from "./pages/services/QualificationValidation/PerformanceQualification";
+import ProcessCleaningValidation from "./pages/services/QualificationValidation/ProcessCleaningValidation";
+import RiskAssessment from "./pages/services/QualificationValidation/RiskAssessment";
+import DevelopingProceduresQms from "./pages/services/DropQM Implementation/DevelopingProceduresQms";
+import TrainingAwareness from "./pages/services/DropQM Implementation/TrainingAwareness";
+import GapAnalysis from "./pages/services/DropQM Implementation/GapAnalysis";
+import InvestigationSupport from "./pages/services/DropQM Implementation/InvestigationSupport";
+import RootCauseAnalysis from "./pages/services/DropQM Implementation/RootCauseAnalysis";
+import QmsMonitoringMeasurement from "./pages/services/DropQM Implementation/QmsMonitoringMeasurement";
+import AssessmentFeasibil from "./pages/services/TechnologyTransferProductDevelopmentSupport/AssessmentFeasibil";
+import ProcessOptimization from "./pages/services/TechnologyTransferProductDevelopmentSupport/ProcessOptimization";
+import RegulatoryCompliances from "./pages/services/TechnologyTransferProductDevelopmentSupport/RegulatoryCompliances";
+
 
 function App() {
       // useEffect(() => {
@@ -77,6 +111,39 @@ function App() {
           <Route path="/rpa" element={<Rpa />} />
           <Route path='/industries' element={<Industries />} />
           <Route path='/parma-audit' element={<PharmaAudit />} />
+          <Route path="/engineer/feasibility-studies" element={<Feasibility />} />
+          <Route path="/engineer/site-selection" element={<SiteSelection />} />
+          <Route path="/engineer/facility-design" element={<FacilityDesign />} />
+          <Route path="/engineer/design-risk" element={<DesignRisk />} />
+          <Route path="/engineer/detailed-engineering" element={<DetailedEngineering />} />
+          <Route path="/engineer/regulatory-compliance" element={<RegulatoryCompliance />} />
+          <Route path="/engineer/project-management" element={<ProjectManagement />} />
+          <Route path="/engineer/technology-equipment" element={<TechnologyEquipment />} />
+          <Route path="/engineer/commissioning-qualification" element={<CommissioningQualification />} />
+          <Route path="/engineer/quality-assurance" element={<QualityAssurance />} />
+          <Route path="/engineer/training-capacity-building" element={<TrainingCapacityBuilding />} />
+          <Route path="/engineer/contractual-closeout" element={<ContractualCloseout />} />
+          <Route path="/engineer/post-project-review" element={<PostProjectReview />} />
+          <Route path="/engineer/supply-chain-management" element={<SupplyChainManagement />} />
+          <Route path="/engineer/financial-analysis" element={<FinancialAnalysis />} />
+          <Route path="/qualificationvalidation/user-requirements-specification" element={<UserRequirementsSpecification />} />
+          <Route path="/qualificationvalidation/design-qualification" element={<DesignQualification />} />
+          <Route path="/qualificationvalidation/factory-site-acceptance" element={<FactorySiteAcceptance />} />
+          <Route path="/qualificationvalidation/installation-qualification" element={<InstallationQualification />} />
+          <Route path="/qualificationvalidation/operation-qualification" element={<OperationQualification />} />
+          <Route path="/qualificationvalidation/standard-operating-procedure" element={<StandardOperatingProcedure />} />
+          <Route path="/qualificationvalidation/performance-qualification" element={<PerformanceQualification />} />
+          <Route path="/qualificationvalidation/process-cleaning-validation" element={<ProcessCleaningValidation />} />
+          <Route path="/qualificationvalidation/risk-assessment" element={<RiskAssessment />} />
+          <Route path="/dropqmsimplementation/developing-procedures-qms" element={<DevelopingProceduresQms />} />
+          <Route path="/dropqmsimplementation/training-awareness" element={<TrainingAwareness />} />
+          <Route path="/dropqmsimplementation/gap-analysis" element={<GapAnalysis />} />
+          <Route path="/dropqmsimplementation/investigation-support" element={<InvestigationSupport />} />
+          <Route path="/dropqmsimplementation/root-cause-analysis" element={<RootCauseAnalysis />} />
+          <Route path="/dropqmsimplementation/qms-monitoring-measurement" element={<QmsMonitoringMeasurement />} />
+          <Route path="/technologytransferproductdevelopmentsupport/assessment-feasibil" element={<AssessmentFeasibil />} />
+          <Route path="/technologytransferproductdevelopmentsupport/process-optimization" element={<ProcessOptimization />} />
+          <Route path="/technologytransferproductdevelopmentsupport/regulatory-compliances" element={<RegulatoryCompliances />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/pages/services/Engineering/Design-Risk.jsx
+++ src/pages/services/Engineering/Design-Risk.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef, } from "react";
+import { useEffect, useRef } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export default function EBMR1() {
+export default function FacilityDesign() {
   const listRefs = useRef([]);
 
   useEffect(() => {
@@ -12,7 +12,7 @@ export default function EBMR1() {
 
   useEffect(() => {
     if (listRefs.current) {
-      ""
+      ("");
     }
     const observers = [];
 
@@ -40,15 +40,20 @@ export default function EBMR1() {
     <>
       <div className="top_container m-3">
         <div className="service_headingr">
-          <h1 className="heading service-head-text" data-aos="fade-down">eBMR</h1>
+          <h1 className="heading service-head-text" data-aos="fade-down">
+            Design Risk Assessment
+          </h1>
         </div>
         <div className="paragraph_container">
-          <p className="paragraph" data-aos="fade-down">
-            At VidyaGxP, we're redefining the landscape of manufacturing intelligence with our pioneering e-BMR
-            solutions. Stepping into the future, we offer more than just software; we provide a gateway to excellence in
-            manufacturing processes. Our e-BMR service is at the heart of this transformation, designed to elevate your
-            operations to new heights of efficiency, compliance, and quality.
-          </p>
+          {/* <p className="paragraph" data-aos="fade-down">
+            At VidyaGxP, we're redefining the landscape of manufacturing
+            intelligence with our pioneering Site Selection solutions. Stepping
+            into the future, we offer more than just software; we provide a
+            gateway to excellence in manufacturing processes. Our Site Selection
+            service is at the heart of this transformation, designed to elevate
+            your operations to new heights of efficiency, compliance, and
+            quality.
+          </p> */}
         </div>
       </div>
       <div className="container-fluid section2 py-5">
@@ -56,25 +61,33 @@ export default function EBMR1() {
           <div className="row g-5 align-items-center row-xm">
             <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
               <div className="abouts" data-aos="fade-right">
-                <img className="img-fluid" src="https://images.pexels.com/photos/5726693/pexels-photo-5726693.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img
+                  className="img-fluid"
+                  //   style={{ backdropFilter: 'none', opacity: 0.5 }}
+                  style={{ width: "100%" }}
+                  src="https://files.oaiusercontent.com/file-0OOQMXceDDZjrDw24yhMyjdd?se=2024-08-06T09%3A38%3A31Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D61943b3a-692b-427c-8cad-103acc269ed0.webp&sig=D0alxpqveJIDpTTrXpHFK8nv5GWMjaG4li8s3KiM6Jo%3D"
+                />
               </div>
             </div>
             <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-              <h4 className="why_line" data-aos="fade-left">Why Choose VidyaGxP’s e-BMR?</h4>
+              <h3 className="why_line" data-aos="fade-left">
+                {/* Why Choose VidyaGxP’s  */}
+                Design Risk Assessment
+              </h3>
               <div className="content_container">
                 <ul>
-                  <li data-aos="fade-left">
-                    <b>Seamless Compliance:- </b>
-                    Navigate the complexities of regulatory standards effortlessly. Our solution ensures your manufacturing
-                    records are in perfect alignment with FDA's 21 CFR Part 11 and other global regulations, guaranteeing audit
-                    success with ease.
+                  <li data-aos="fade-left" className=" fs-5">
+                    {/* <b>Seamless Compliance:- </b> */}
+                    Identifying and mitigating potential hazards, contamination
+                    risks, and operational inefficiencies thorough risk
+                    assessments.
                   </li>
-                  <li data-aos="fade-left">
+                  {/* <li data-aos="fade-left">
                     <b>Unmatched Efficiency:-</b>
                     Eliminate manual errors and reduce processing times with our automated, AI-driven platform. VidyaGxP
                     turns your data into actionable insights, streamlining workflows and boosting productivity.
-                  </li>
-                  <li data-aos="fade-left">
+                  </li> */}
+                  {/* <li data-aos="fade-left">
                     <b>Robust Data Integrity:-</b>
                     Safeguard your critical manufacturing data with state-of-the-art security measures. Our electronic batch
                     records offer unparalleled transparency, traceability, and trust, thanks to advanced encryption, electronic
@@ -89,7 +102,7 @@ export default function EBMR1() {
                     <b>Scalable Solutions:-</b>
                     Whether you're a start-up or an established enterprise, VidyaGxP grows with you. Our flexible, scalable
                     solutions can be tailored to fit your unique needs, ensuring you're always ahead of the curve.
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </div>
@@ -97,7 +110,7 @@ export default function EBMR1() {
         </div>
       </div>
 
-      <div className="middle_line_container m-3" data-aos="zoom-in">
+      {/* <div className="middle_line_container m-3" data-aos="zoom-in">
         <h4 className="middle_line">Join the VidhaGxP Revolution</h4>
         <p>
           Embrace the future with VidyaGxP and transform your electronic batch manufacturing records into a
@@ -105,13 +118,13 @@ export default function EBMR1() {
           you partner with VidyaGxP, you're not just optimizing your operations; you're setting a new standard for
           the industry.
         </p>
-      </div>
+      </div> */}
 
-      <div className="bottom_line_container">
+      {/* <div className="bottom_line_container">
         <h5 className="bottom_line" data-aos="zoom-in">
           "Discover the VidyaGxP difference. Elevate your manufacturing process now."
         </h5>
-      </div >
+      </div > */}
     </>
   );
 }
